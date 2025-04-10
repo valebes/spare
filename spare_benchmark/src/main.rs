f545c306f4c7d4f6c625ccb1e95fa6773faf0a22
Ubuntu <ubuntu@n16.maas>
2025-04-10 17:17:07 +0000

    Update parameters for the test

--- spare_benchmark/src/main.rs
+++ spare_benchmark/src/main.rs
@@ -107,9 +107,9 @@ async fn test(
     function_path: &String,
     payload: &Option<String>,
 ) -> (u128, usize, usize, Vec<u128>) {
-    let request_per_epoch = ((16 * nodes.len()) as f32 * 0.5).floor() as usize; // 100% Load
+    let request_per_epoch = ((16 * nodes.len()) as f32 * 1.0).floor() as usize; // 100% Load
 
-    let inter_arrival = 8; // ms
+    let inter_arrival = 1; // ms
     let mut latency_per_epoch = Vec::new();
     let latency = Arc::new(Mutex::new(Vec::new()));
     let completed = Arc::new(AtomicUsize::new(0));
@@ -146,7 +146,7 @@ async fn test(
                 let invoke_function = InvokeFunction {
                     function: "test".to_string(), // Function name (This is hardcoded for now)
                     image: function_path_tmp,
-                    vcpus: 2,
+                    vcpus: 1,
                     memory: 512,
                     payload: payload_clone,
                     emergency: false,
