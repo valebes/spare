9472094dd583a727492bc46d4f90c34cdf773673
Ubuntu <ubuntu@n16.maas>
2025-04-11 14:11:46 +0000

    Update parameters for the test

--- spare_benchmark/src/main.rs
+++ spare_benchmark/src/main.rs
@@ -107,9 +107,9 @@ async fn test(
     function_path: &String,
     payload: &Option<String>,
 ) -> (u128, usize, usize, Vec<u128>) {
-    let request_per_epoch = ((16 * nodes.len()) as f32 * 1.0).floor() as usize; // 100% Load
+    let request_per_epoch = ((16 * nodes.len()) as f32 * 0.5).floor() as usize; // 100% Load
 
-    let inter_arrival = 3; // ms
+    let inter_arrival = 30; // ms
     let mut latency_per_epoch = Vec::new();
     let latency = Arc::new(Mutex::new(Vec::new()));
     let completed = Arc::new(AtomicUsize::new(0));
