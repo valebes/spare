e062d57a1766f24dca701608732697751c7bf0bb
Ubuntu <ubuntu@n16.maas>
2025-04-13 16:32:38 +0000

    Debugging

--- spare/src/ohsw/src/utils/socket.rs
+++ spare/src/ohsw/src/utils/socket.rs
@@ -17,7 +17,7 @@ pub async fn read_exact(stream: &mut UnixStream, buf: &mut [u8]) -> Result<(), s
                 if e.kind() != std::io::ErrorKind::WouldBlock {
                     return Err(e);
                 } else {
-                    yield_now().await;
+                    sleep(std::time::Duration::from_millis(5)).await;
                     continue;
                 }
             }
@@ -43,7 +43,7 @@ pub async fn write_all(stream: &mut UnixStream, buf: &[u8]) -> Result<(), std::i
                 if e.kind() != std::io::ErrorKind::WouldBlock {
                     return Err(e);
                 } else {
-                    yield_now().await;
+                    sleep(std::time::Duration::from_millis(5)).await;
                     continue;
                 }
             }
