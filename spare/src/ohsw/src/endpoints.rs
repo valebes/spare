2f46fbe1f83a4f1b346586fcda2772d32a53682d
Ubuntu <ubuntu@n16.maas>
2025-04-13 14:43:21 +0000

    Debugging

--- spare/src/ohsw/src/endpoints.rs
+++ spare/src/ohsw/src/endpoints.rs
@@ -236,7 +236,7 @@ async fn start_instance(
 
             info!("Starting instance: {} ip: {}", instance.id, instance.ip);
 
-            let mut stream = match timeout(Duration::from_millis(500), socket.accept()).await {
+            let mut stream = match timeout(Duration::from_millis(100), socket.accept()).await {
                 Ok(res) => match res {
                     Ok((stream, _)) => stream,
                     Err(e) => {
@@ -259,6 +259,7 @@ async fn start_instance(
                 instance.id
             );
 
+            sleep(Duration::from_millis(30)).await;
 
             let mut buf = [0; 5];
             // Read from the vsock socket
